e1d547c41e12dfb22e88ffadb1835c5124e112bd
Matt <matthv@gmail.com>
2022-10-18 16:30:07 +0200

    fix: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/create'
-  get 'products/new'
   mount ForestLiana::Engine => '/forest'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
